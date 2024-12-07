6718743e67814bd84b81494ff8d3ca50b6c3f17c
Nimares <nikolai.resvold@gmail.com>
2024-12-07 13:34:04 +0100

    initial commit: new repo for library project

---
+++ README.md
@@ -0,0 +1,38 @@
+TO-DO: HTML/JS 
+1. [X] func: addBookToLibrary input to add to library (adds to array) []
+2. [X] func: displayBooks display library in table or on cards
+3. [X] func: newBook
+    * form
+    * author, title, number of pages, whether it’s been read
+    * seperated displayBook, and displayLibrary. 
+4. [X] func: deleteBook
+    - Now functioning
+    - Was facing issues, i was trying to apply event listener before the elements were present in the DOM
+    - fixed by adding event listener to each book delete button on creation.
+    - also faced issues with using delete from object / remove from dom (i needed a break)
+5. [X] func: updateReadStatus
+    Goal: Button that changes appearance based on read status of each book. 
+    Progress:
+        - 05.12.24: 
+            * For toggling classes, you should either toggle in style of dark
+                mode (default style vs dark mode) or have function check read read-status boolean
+                and then react based on this seperated style classes for button. 
+            * 1 class for button style, and 2 different classes based on read-status
+            * read-status button color edited via css 
+6. style: General page layout
+7. optional: darkmode & responsive design. 
+8. final: review and assessment 
+    Goal: 
+        1 - HTML/CSS checking.
+        2 - Refactor 
+        3 - Look at other students projects
+        4 - Closing thoughts
+
+Optional: 
+- try some of the css methods you've seen for responsive design (link to video)
+- try some css methods from previous lessons
+- add icons (not svg)
+- try dark/light mode
+
+Approach: 
+    
---
+++ script.js
@@ -0,0 +1,130 @@
+// client side storage object
+const myLibrary = {
+    book1: { title: "book1", author: "JRR", pages: 1137, read: false },
+    book2: { title: "book2", author: "book2-author", pages: 666, read: false },
+    book3: { title: "book3", author: "book3-author", pages: 666, read: false },
+    book4: { title: "book4", author: "book4-author", pages: 666, read: false },
+}
+
+const library = document.querySelector(".library");
+const bookDialog = document.querySelector("dialog");
+const showButton = document.querySelector(".add");
+const confirmBtn = bookDialog.querySelector("#confirmBtn");
+const titleInput = bookDialog.querySelector("#bookTitle");
+const authorInput = bookDialog.querySelector("#bookAuthor");
+const pagesInput = bookDialog.querySelector("#bookPages");
+const checkedStatus = bookDialog.querySelector("#bookRead");
+const outputBox = document.querySelector("output")
+
+displayLibrary();
+
+// "Show the dialog" button opens the dialog modally
+showButton.addEventListener("click", () => {
+    bookDialog.showModal();
+});
+
+// "Close" button closes the dialog, creates book object, ads book to DOM and storage.
+confirmBtn.addEventListener("click", (event) => {
+    // Prevent page reload, and form submission
+    event.preventDefault();
+    const newBook = new Book(
+        `${titleInput.value}`,
+        `${authorInput.value}`,
+        `${pagesInput.value}`,
+        checkedStatus.checked,
+
+    )
+    myLibrary[`${newBook.title}`] = newBook;
+    displayBook(newBook);
+    bookDialog.close();
+});
+
+// Book constructor function
+function Book(title, author, pages, read) {
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+    this.read = read;
+}
+
+// Loops through myLibrary object, and displays each book on the page
+function displayLibrary() {
+    for (const book in myLibrary) {
+        displayBook(myLibrary[book])
+    }
+}
+
+// assigns random book style to each book added
+function generateRandomStyle() {
+    let randomInt = Math.floor(Math.random() * (3) + 1);
+    if (randomInt === 1) {
+        return "green-book"
+    } else if (randomInt === 2) {
+        return "red-book"
+    } else {
+        return "blue-book"
+    }
+}
+
+// Adds book to library display
+function displayBook(book) {
+    const bookDisplay = document.createElement("div");
+    const bookTitle = document.createElement("h2");
+    const bookAuthor = document.createElement("p");
+    const bookPages = document.createElement("p");
+    const bookReadStatus = document.createElement("p");
+
+    // Need to add event listener on creation.
+    // Trying to add eventlistener after creation caused issues
+    const deleteBtn = document.createElement("button");
+    deleteBtn.classList.add("deleteBtn");
+
+    // Delete based on parent node
+    deleteBtn.addEventListener("click", (e) => {
+        delete myLibrary[e.target.parentNode.firstChild.innerHTML];
+        e.target.parentNode.remove();
+    }
+    );
+
+
+    const readStatusBtn = document.createElement("button");
+
+    // 05.12.24 add default class for read status (for use later for "on") 
+    readStatusBtn.classList.add("read-status");
+
+    if (book.read === true) {
+        readStatusBtn.classList.add("read");
+    } else {
+        readStatusBtn.classList.add("not-read");
+    }
+
+
+    // Simplified function. Alter read status then toggle both classes
+    readStatusBtn.addEventListener("click", () => {
+        if (book.read === true) {
+            book.read = false
+        } else {
+            book.read = true
+        }
+        readStatusBtn.classList.toggle("not-read");
+        readStatusBtn.classList.toggle("read");
+    })
+
+
+    bookTitle.innerHTML = book.title;
+    bookAuthor.innerHTML = book.author;
+    bookPages.innerHTML = book.pages;
+    bookReadStatus.innerHTML = book.read;
+
+    bookDisplay.classList.add("book");
+    bookDisplay.classList.add(generateRandomStyle());
+
+    bookDisplay.appendChild(bookTitle);
+    bookDisplay.appendChild(bookAuthor);
+    bookDisplay.appendChild(bookPages);
+    bookDisplay.appendChild(bookReadStatus);
+    bookDisplay.appendChild(deleteBtn);
+    bookDisplay.appendChild(readStatusBtn);
+    library.appendChild(bookDisplay);
+
+}
